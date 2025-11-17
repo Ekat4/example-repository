8289feacfefaf789de4d958dae3e4f374fdbf510
Ekaterina <92938574+Ekat4@users.noreply.github.com>
2025-11-17 07:29:39 -0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;  // Реализуй как a * b, а не 0.0
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+}
